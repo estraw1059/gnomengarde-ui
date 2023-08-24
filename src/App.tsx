f9a560a99b308fcce0a6967d277b376f11aed912
Eric <estraw@smu.edu>
2023-08-24 06:36:30 -0500

    Additional Clean-up/planning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [playerName, setPlayerName] = useState();
+
+  //This app will have two components:
+  //1. Login: This will take the players name and log them into the room. 
+  //2. Room Selection: This will have a room input which will spit back info
+
   return (
     <div className="App">
       Basic App

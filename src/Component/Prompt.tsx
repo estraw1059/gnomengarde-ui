5ed7ba45bf6c21c6383ec215aca8ed81c31857a4
Eric <estraw@smu.edu>
2023-08-29 20:03:03 -0500

    Played around with CSS

--- src/Component/Prompt.tsx
+++ src/Component/Prompt.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
+
+import React, { CSSProperties } from 'react';
+import { Button, Container, Form, Row } from 'react-bootstrap';
+import './styles.css';
+
+interface PromptStyle {
+    containerStyle: CSSProperties;
+}
+
 
 const Prompt = () => {
     return (
-        <Container>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-
+                <Form className='container mt-3 mb-3'>
+                    <Row className="mb-3">
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+                    </Row>
+                    <Row className='mb-3'>
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" />
                     </Form.Group>
+                    </Row>
 
                     <Form.Group controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Check me out" />
@@ -25,8 +31,7 @@ const Prompt = () => {
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
-            </Form>
-        </Container>
+                </Form>
     );
 };
 
